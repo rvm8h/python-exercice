10968f97de8d8c4229620a0656ed4646b04d9675
Jérémy Tran <tran.jeremy.av@gmail.com>
2018-03-21 21:19:47 +0100

    last commit until the next one

--- exemple_4_OT.py
+++ exemple_4_OT.py
@@ -4,29 +4,42 @@
 #trier le dictionnaire par valeur
 import re
 
+#opening input file
 file = open('data.txt', 'r')
 #print(file.read())
 
+#reading input file
 List = file.read()
-regex = r"(\w*),(.*)\n?"
 
-test_str = ("valeur, 456\nvaleur, 32\nvaleur, 5")
+#regex declaration
+regex = r"(.*),(.*)\n?"
 
-matches = re.finditer(regex, test_str)
+#regex matches iterations
+matches = re.finditer(regex, List)
+
+#dictionary declaration
 Dickus = dict()
+
+#input values into dictionary
 for matchNum, match in enumerate(matches):
     matchNum = matchNum + 1
 
     for groupNum in range(0, len(match.groups())):
+        if groupNum == 1 :
+            a = match.group(groupNum).strip('\n')
+
         groupNum = groupNum + 1
 
-        print("Group {groupNum} found: {group}".format(groupNum=groupNum, group=match.group(groupNum)))
-        if groupNum == 2:
-            a = match.group(groupNum - 1)
-            b = match.group(groupNum)
-            print(a,',',b)
-            Dickus[a] = b
+        #print("Group {groupNum} found: {group}".format(groupNum=groupNum, group=match.group(groupNum)))
 
-print(sorted(Dickus))
+        if groupNum == 2:
+            b = match.group(groupNum).strip(r'\n')
+            print('a =',a,',b =',b)
+            Dickus[a] = int(b)
+
+        #print(Dickus)
+
+#output sorted dictionary
+print('Dictionary:',Dickus,'\nSorted Dictionary:',sorted(Dickus))
 
 file.close()
