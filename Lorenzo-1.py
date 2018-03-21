3d4d32ad19887bb12f522e4edd741b360337ee88
Lorenzo <lorenzo.it@outlook.com>
2018-03-21 17:42:01 +0100

    Exercise (second version)

--- Lorenzo-1.py
+++ Lorenzo-1.py
@@ -2,8 +2,27 @@
 # et mettre les valeurs dans un dictionnaire
 
 l = []
-
 for line in open('data.txt'):
         l.append(int(line.split(',')[1]))
 d={'Valeurs': l}
 print(d)
+
+#Second version
+dic = dict()
+file = open('data.txt', 'r')
+txt = file.read()
+dic = dict()
+lst = txt.split('\n')
+
+print('List : ', lst)
+
+for line in lst:
+    item = line.split(',')
+    dic[item[0]] = int(item[1])
+
+print('Dic : ', dic)
+
+for key in sorted(dic):
+    print(key, '=>', dic[key])
+
+file.close()
