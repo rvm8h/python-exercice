af8164de7c547d3f634a2b1beb0c8f3827d65202
Lorenzo Esposito <lorenzo.esposito@free.fr>
2018-03-22 13:01:38 +0100

    Exercise 3 Boucles while

--- Lorenzo-1.py
+++ Lorenzo-1.py
@@ -44,24 +44,30 @@ for row in lines:
     val = int(item[1].rstrip())
     dic[item[0]] = int(item[1])
 
-print(dic)
-
 for key in sorted(dic):
     print(key, '=>', dic[key])
+
+tup = [(key, val) for key, val in dic.items()]
+
+tup.sort()
+
+dic = dict((x, y) for x, y in tup)
+
+print('Dic trié par clé : ', dic)
 
 tup = [(val, key) for key, val in dic.items()]
 
 tup.sort()
 
-print (tup)
+#print (tup)
 
 tup = [(key, val) for val, key in tup]
 
-print (tup)
+#print (tup)
 
-dicT = dict((y, x) for x, y in tup)
+dic = dict((y, x) for x, y in tup)
 
-print(dicT)
+print('Dic trié par valeur : ', dic)
 
 # Exercise 2 Création de fichier avec uniquement des WARNING
 lst=[]
@@ -74,3 +80,20 @@ for line in lines:
 out = open('lorenzo.ret', 'w')
 out.writelines(lst)
 out.close()
+
+# Exercise 3 Boucles while
+# Initialisez deux entiers : a = 0 et b = 10.
+# Écrire une boucle affichant et incrémentant la valeur de a tant qu’elle reste inférieure à celle de b.
+# Écrire une autre boucle décrémentant la valeur de b et affichant sa valeur si elle est impaire. Boucler tant que b n’est pas nul.
+
+a = 0
+b = 10
+
+while a < b :
+    a += 1
+    print("Valeur de a : ", a)
+
+while b > 0 :
+    b -= 1
+    if b%2!=0:
+        print("Valeur de b : ", b)
