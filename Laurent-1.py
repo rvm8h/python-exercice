3e466da1c8ff6ecb84b2d369f1abd3704ccc6905
Laurent Monribot <laurent.monribot@etu.u-pec.fr>
2018-03-22 12:13:57 +0100

    Ajout tri du dictionnaire par les quantités

--- Laurent-1.py
+++ Laurent-1.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python3
 
-#Mettre les lignes du fichier texte dans un dictionnaire
-#Version avec un type d'objet unique "valeur" recherché sur toutes les lignes du fichier
-l=[]
-mydic={}
-#close implicite dans le for
-for line in open('data.txt'):
-    if 'valeur' == line.split(',')[0] :
-        l.append(int(line.split(',')[1]))
-l.sort()
-mydic={'valeur':l}
-print(mydic)
-
 
 #Version avec des types d'objets différents triés par noms, chacun avec leur nombre :
 lTypes=[]
@@ -27,7 +15,23 @@ for key in sorted(mydic):
     lNumbers.append(mydic[key])
 lTypes.sort()
 mydic=dict(zip(lTypes, lNumbers))
-print(mydic)
+print('Dictionnaire trié par noms : ', mydic)
+
+
+
+#Version avec des types d'objets différents triés par quantités :
+lTypes=[]
+lNumbers=[]
+mydic={}
+for line in open('data.txt'):
+        lTypes.append(line.split(',')[0].replace(' ',''))
+        lNumbers.append(int(line.split(',')[1]))
+mydic=dict(zip(lTypes, lNumbers))
+mytuples=[(v, k) for k, v in mydic.items()]
+mytuples.sort()
+mydic=dict([(v, k) for k, v in mytuples])
+print('Dictionnaire trié par quantités : ', mydic)
+
 
 
 #Création d'un fichier ne contenant que les chaines WARNING du fichier jenkins.dat
@@ -36,7 +40,6 @@ with open('jenkins.dat', 'r', encoding='utf-8') as fIn:
     for line in fIn.readlines():
         if "WARNING" in line:
             l.append(line)
-fOut=open('laurent.ret', 'w', encoding='utf-8')
-fOut.writelines(l)
-fOut.close()
+with open('laurent.ret', 'w', encoding='utf-8') as fOut:
+    fOut.writelines(l)
 
