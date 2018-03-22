4f3124caf30a58077a2cb19b40d467aac39a1614
Jérémy Tran <tran.jeremy.av@gmail.com>
2018-03-22 11:53:04 +0100

    V3 improved

--- exemple_4_OT.py
+++ exemple_4_OT.py
@@ -29,14 +29,14 @@ for matchNum, match in enumerate(matches):
 
     for groupNum in range(0, len(match.groups())):
         if groupNum == 1 :
-            a = match.group(groupNum).strip("\n").replace(" ","")
+            a = match.group(groupNum).rstrip().replace(" ","")
 
         groupNum = groupNum + 1
 
         #print("Group {groupNum} found: {group}".format(groupNum=groupNum, group=match.group(groupNum)))
 
         if groupNum == 2:
-            b = match.group(groupNum).strip("\n").replace(" ","")
+            b = match.group(groupNum).rstrip().replace(" ","")
             print('a =',a,',b =',b)
             Dickus[a] = int(b)
 
@@ -60,7 +60,7 @@ Dickus = dict()
 
 #deleting \n
 for x in range(len(lines)):
-   lines[x] = lines[x].strip("\n").split(",")
+   lines[x] = lines[x].rstrip().split(",")
 
    for y in range(len(lines[x])):
        lines[x][y] = lines[x][y].replace(" ","")
@@ -118,7 +118,7 @@ Dickus = dict()
 
 #deleting \n
 for x in range(len(lines)):
-   lines[x] = lines[x].strip("\n").split(",")
+   lines[x] = lines[x].rstrip().split(",")
 
    for y in range(len(lines[x])):
        lines[x][y] = lines[x][y].replace(" ","")
@@ -127,10 +127,20 @@ for x in range(len(lines)):
 for x in range(len(lines)):
     Dickus[lines[x][0]] = int(lines[x][1])
 
+#sort by key
+SKDickus = dict()
+
+for x in sorted(Dickus):
+    SKDickus[x] = Dickus[x]
+
+#sort by value
 sortval = [(v,k) for k,v in Dickus.items()]
-
 sortval.sort()
-
 sortval = [(k,v) for v,k in sortval]
+
+SVDickus = dict()
+for x in range(len(sortval)):
+    SVDickus[sortval[x][0]] = sortval[x][1]
+
 #output
-print('\nDictionary:', Dickus, '\nSorted by keys:', sorted(Dickus),'\nSorted by values',sortval)
+print('\nDictionary:', Dickus, '\nSorted by keys:', SKDickus,'\nSorted by values',SVDickus)
