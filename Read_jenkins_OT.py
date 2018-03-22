569b3c3596cc873fc144539540df1b46371eab5a
Jérémy Tran <tran.jeremy.av@gmail.com>
2018-03-22 11:24:01 +0100

    code esthetic improvements

--- Read_jenkins_OT.py
+++ Read_jenkins_OT.py
@@ -3,31 +3,27 @@
 #avec une liste comprehension selectionner que les lignes qui contiennent WARNING
 #ecrire toutes ces lignes dans le fichier <votre_nom>.ret
 
-
+#files setting
 file = open("jenkins.dat","r")
 output = open("Tran_Olivier.ret", "w")
+
 #split file in lines
 data = file.read().split("\n")
-print(data)
+
 #split lines in sub elements
 for x in range(len(data)):
 
     data.append(data[x].split("\t"))
 
-#looking for warnings and writing new file
+#looking for warnings and writing in new file
 for x in range(len(data)):
 
     for y in range(len(data[x])):
+
         if data[x][y].find("WARNING") != -1:
+
             print("warning found:",str(data[x]).replace("[","").replace("]",""))
             output.write(str(data[x]).replace("[","").replace("]",""))
-
-
-print(data)
-
-
-
-
 
 
 
