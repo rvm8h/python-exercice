0b839e326ca1a9a164f53b8072ed0300422d8dfa
TranOlivier <olivier.tran.al@gmail.com>
2018-03-23 14:49:25 +0100

    V1.0

---
+++ email_OT.py
@@ -0,0 +1,25 @@
+#creer une liste contacts vide
+#ouvrir le fichier email.txt en lecture
+#lire la premiere ligne en applicant strip et split
+#vous avez ainsi une liste contenant les headers
+#lire les lignes suivantes en applicant la fonction zip ( header, line)
+#faire un append dans votre liste contacts du resultat de la fonction zip
+#afficher le resultat au format
+#email: <value>  --  lastname ,  firstname
+#utiliser l'outil python console pour mettre au point votre structure de donnees
+
+
+
+with open("email.txt","r") as mail:
+    LContacts = list()
+    list = mail.readlines()
+    header = list[0].rstrip().split(",")
+    print(header)
+    for l in list:
+        if "first" not in l:
+            LContacts.append(zip(header,l.rstrip().split(",")))
+
+    print(LContacts)
+    for l in LContacts:
+        H,F = zip(*l)
+        print("email:" , F[2] , "--" , F[1], F[0])
