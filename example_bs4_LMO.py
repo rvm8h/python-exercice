0bc39c5a2b09c8aeed7b152e02ec5efce1051831
Laurent <laurent.monribot@gmail.com>
2018-03-27 16:33:59 +0200

    Exemples simples Beautifulsoup4

--- example_bs4_LMO.py
+++ example_bs4_LMO.py
@@ -2,19 +2,19 @@ from bs4 import BeautifulSoup
 import requests
 
 
-# helloworld = "<p>Hello World</p>"
-# soup_string = BeautifulSoup(helloworld, "html.parser")
-# print(soup_string)
-#
-#
-# url = "https://www.barnesandnoble.com/b/barnes-noble-classics/_/N-rqv"
-# response = requests.get(url)
-# soup_page = BeautifulSoup(response.content,"html.parser")
-# print(soup_page)
-#
-# with open("foo.html") as foo_file:
-# 	soup = BeautifulSoup(foo_file, "html.parser")
-# print(soup)
+helloworld = "<p>Hello World</p>"
+soup_string = BeautifulSoup(helloworld, "html.parser")
+print(soup_string)
+
+
+url = "https://www.barnesandnoble.com/b/barnes-noble-classics/_/N-rqv"
+response = requests.get(url)
+soup_page = BeautifulSoup(response.content,"html.parser")
+#print(soup_page)
+
+with open("foo.html") as foo_file:
+	soup = BeautifulSoup(foo_file, "html.parser")
+print(soup)
 
 
 html_atag = """<html><body><p>Test html a tag example</p>
@@ -27,6 +27,17 @@ atag = soup.a
 print(atag)
 for atag in soup.findAll('a'):
     print(atag.string)
-    print(atag.target)
 
 print(type(atag))
+
+html_identical = """<p class="identical">
+Example of p tag with class identical
+</p>
+<div class="identical">
+Example of div tag with class identical
+</div>"""
+soup = BeautifulSoup(html_identical, "html.parser")
+identical_div = soup.find("div", class_="identical")
+print(identical_div)
+
+
