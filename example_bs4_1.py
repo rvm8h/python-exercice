c575776dfa62558430b73e323ca30f3c24865f21
Herve <dockerlite@gmail.com>
2018-03-27 16:42:19 +0200

    add bs4 +

--- example_bs4_1.py
+++ example_bs4_1.py
@@ -41,3 +41,6 @@ with open("ecologicalpyramid.html") as ecological_pyramid:
     soup = BeautifulSoup(ecological_pyramid, "html.parser")
 producer_entries = soup.find("ul")
 print(producer_entries.li.div.string)
+t =soup.findAll("ul")
+for v in t:
+    print(v.li.div.string)
