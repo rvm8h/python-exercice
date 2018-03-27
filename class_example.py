a8f5fae36a8dffedb3b92e70896a862e18299c9e
Laurent <laurent.monribot@gmail.com>
2018-03-27 11:12:17 +0200

    Ajout employee_LMO.py

---
+++ class_example.py
@@ -0,0 +1,45 @@
+
+
+class FirstClass:
+    def setdata(self, value):
+        self.data = value
+    def display(self):
+        print(self.data)
+
+
+#Appel des constructeurs
+x=FirstClass()
+y=FirstClass()
+
+x.setdata("Thomas")
+y.setdata(3.1415)
+
+x.display()
+y.display()
+
+class SecondClass(FirstClass):
+    def display(self):
+        print("Current value : {}".format(self.data))
+
+z = SecondClass()
+z.setdata(456)
+z.display()
+
+class ThirdClass(SecondClass):
+    def __init__(self, value):
+        self.data = value
+    def __add__(self, other):
+        return ThirdClass(self.data + other)
+    def __str__(self):
+        return '[ThirdClass : {} ]'.format(self.data)
+    def mul(self, other):
+        self.data *= other
+
+a = ThirdClass('abc')
+a.display()
+print(a)
+b = a + 'xyz'
+b.display()
+print(b)
+
+
