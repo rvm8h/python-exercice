8606c643059f55966eaf635bac3b3223c4a88ba6
Laurent <laurent.monribot@gmail.com>
2018-03-27 16:24:55 +0200

    Merge branch 'master' of https://github.com/rvm8h/python-exercice
    # Conflicts:
    #	class_example.py

---
+++ example_bs4_LMO.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+# helloworld = "<p>Hello World</p>"
+# soup_string = BeautifulSoup(helloworld, "html.parser")
+# print(soup_string)
+#
+#
+# url = "https://www.barnesandnoble.com/b/barnes-noble-classics/_/N-rqv"
+# response = requests.get(url)
+# soup_page = BeautifulSoup(response.content,"html.parser")
+# print(soup_page)
+#
+# with open("foo.html") as foo_file:
+# 	soup = BeautifulSoup(foo_file, "html.parser")
+# print(soup)
+
+
+html_atag = """<html><body><p>Test html a tag example</p>
+<a href="http://www.test.com">Home</a>
+<a href="http;//www.test.com/books">Books</a>
+</body>
+</html>"""
+soup  = BeautifulSoup(html_atag,"html.parser")
+atag = soup.a
+print(atag)
+for atag in soup.findAll('a'):
+    print(atag.string)
+    print(atag.target)
+
+print(type(atag))
