92b0937d68dab8a02a23171b4814849480c4917a
Olivier <olivier.tran.al@gmail.com>
2018-03-27 14:48:20 +0200

    V1.0 rectangle et employee

---
+++ rectangle_OT.py
@@ -0,0 +1,42 @@
+# creer une class Retangle
+# prend 2 parameters long , larg , par default  = 0
+# une methode surface
+# une methode __str__ pour l 'affichage avec
+# ces caracteriques lon , larg ,surface
+
+
+
+class rectangle:
+
+    def __init__(self, long = 0, larg = 0):
+        self.long = long
+        self.larg = larg
+        self.nom = "rectangle"
+
+    def surface(self):
+
+        self.surface = self.larg * self.long
+        return self.surface
+
+    def __str__(self):
+        return "{} Longeur: {}, Largeur: {}, Surface: {}".format(self.nom, self.long, self.larg, self.surface())
+
+class carre(rectangle):
+
+    def __init__(self, cote = 0):
+        rectangle.__init__(self,cote, cote)
+        self.nom = "carré"
+
+
+
+x = rectangle()
+y = rectangle(3,2)
+z = carre(2)
+
+
+
+
+print(x)
+print(y)
+print(z)
+
