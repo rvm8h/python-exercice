c9c119e4d137bf3558b15d8a0155c0d565c0702a
Olivier <olivier.tran.al@gmail.com>
2018-03-27 15:34:41 +0200

    modif de rectangle_ot

--- rectangle_OT.py
+++ rectangle_OT.py
@@ -1,7 +1,6 @@
 # creer une class Retangle
 # prend 2 parameters long , larg , par default  = 0
 # une methode surface
-<<<<<<< HEAD
 # une methode __str__ pour l 'affichage avec
 # ces caracteriques lon , larg ,surface
 
@@ -40,29 +39,3 @@ z = carre(2)
 print(x)
 print(y)
 print(z)
-
-=======
-#une methode __str__ pour l 'affichage avec
-#ces caracteriques lon , larg ,surface
-
-
-
-class rectangle():
-    def __init__(self, long = 0, larg = 0):
-        self.long = long
-        self.larg = larg
-
-    def surface(self):
-        self.surface = self.larg * self.long
-
-    def __str__(self):
-        return "Longeur: {}, Largeur: {}, Surface: {}".format(self.long, self.larg, self.surface)
-
-x = rectangle()
-x.surface()
-
-y = rectangle(3,2)
-y.surface()
-
-print("x =", x ,"\n y =", y)
->>>>>>> origin/Olivier
