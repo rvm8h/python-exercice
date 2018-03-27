4e2b61b7031372f1728c82c75a038507e278a438
Lorenzo <lorenzo.it@outlook.com>
2018-03-27 16:24:41 +0200

    BS4

---
+++ example_bs4_LES.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+helloworld = "<p>Hello World</p>"
+soup_string = BeautifulSoup(helloworld, "html.parser")
+print(soup_string)
+
+
+url = "https://www.barnesandnoble.com/b/barnes-noble-classics/_/N-rqv"
+response = requests.get(url)
+soup_page = BeautifulSoup(response.content,"html.parser")
+print(soup_page)
+
+with open("foo.html") as foo_file:
+	soup = BeautifulSoup(foo_file, "html.parser")
+print(soup)
+
+
+html_atag = """<html><body><p>Test html a tag example</p>
+<a href="http://www.test.com">Home</a>
+<a href="http;//www.test.com/books">Books</a>
+</body>
+</html>"""
+soup  = BeautifulSoup(html_atag,"html.parser")
+atag = soup.a
+atags = soup.findAll('a')
+print(atag)
+print(atags[1])
+print(type(atags))
+
+for link in atags:
+    print(link.string)
+
+ptag = soup.p
+print(ptag)
+
