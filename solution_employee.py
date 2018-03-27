3a629f2dd3e9ec260b0ebdd9799dad4376bb6460
Herve <dockerlite@gmail.com>
2018-03-27 11:02:07 +0200

    add class example + exercice

---
+++ solution_employee.py
@@ -0,0 +1,5 @@
+# creer une classe employee
+# avec une methode __init__ qui prend  nmae et salary comme parameters
+# creer une variable de classe globale qui compte les employes , faire des recherches google
+# creer une methode displaycount qui affiche le nombre d'employes
+# creer une methode display qui affiche un employe
