e3703d19924ff22e4696e5f71ae174362bc891a5
Laurent <laurent.monribot@gmail.com>
2018-03-28 13:59:57 +0200

    Affichage stations métro + lignes

--- example_bs4_LMO.py
+++ example_bs4_LMO.py
@@ -82,7 +82,6 @@ print(all_texts_in_list)
 
 
 
-
 url = r"https://en.wikipedia.org/wiki/List_of_stations_of_the_Paris_M%C3%A9tro"
 response = requests.get(url)
 soup_page = BeautifulSoup(response.content,"html.parser")
@@ -91,7 +90,6 @@ for soup_line in soup_table.findAll('tr'):
     indice = 0
     for soup_case in soup_line.findAll('td'):
         for soup_station in soup_case.findAll('a'):
-            if soup_station.has_attr('href') and soup_station.has_attr('title'):
-                if((indice == 0 or indice ==3) and not soup_station.string == None):
-                    print(soup_station.string)
+            if((indice == 0 or indice ==3) and not soup_station.string == None):
+                print(soup_station.string)
         indice+=1
