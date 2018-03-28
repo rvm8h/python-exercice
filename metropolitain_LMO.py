3a33d4e4d91dba1fab72bdfd041466b930d17607
Laurent <laurent.monribot@gmail.com>
2018-03-28 14:08:40 +0200

    Affichage stations métro + lignes

--- metropolitain_LMO.py
+++ metropolitain_LMO.py
@@ -1,6 +1,8 @@
 
 #Récupération des stations de métro de Paris avec les lignes qui passent
 
+from bs4 import BeautifulSoup
+import requests
 
 url = r"https://en.wikipedia.org/wiki/List_of_stations_of_the_Paris_M%C3%A9tro"
 response = requests.get(url)
