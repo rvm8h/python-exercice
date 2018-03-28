a4199975d30ae8c77c6f6e4629caf17d826a98f1
Lorenzo <lorenzo.it@outlook.com>
2018-03-28 14:12:01 +0200

    Metropolitain Exercise

---
+++ metropolitain_LES.py
@@ -0,0 +1,20 @@
+# Trouver tous les tag th dans le site web
+# url = r"https://en.wikipedia.org/wiki/List_of_stations_of_the_Paris_M%C3%A9tro"
+# Dans ce site web travailler avec les tags th et tr pour retourner tous les noms des stations de metros et son numero de ligne associe. choisir son format de donnees
+
+from bs4 import BeautifulSoup
+import requests
+
+url = r"https://en.wikipedia.org/wiki/List_of_stations_of_the_Paris_M%C3%A9tro"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+
+table = soup.find('table')
+
+#print(table)
+
+for tr in table.findAll('tr'):
+
+    for td in tr.findAll('td')[::3]:
+        for a in td.findAll('a'):
+            print(a.text)
