a6768c6c4f1104cb58bbde2714a0e33b36d6036e
Laurent <laurent.monribot@gmail.com>
2018-03-28 16:10:17 +0200

    Mise en place d'un serveur localhost:5000

--- metropolitain_LMO.py
+++ metropolitain_LMO.py
@@ -4,7 +4,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-data= []
+stationslist= []
+lineslistlist= []
 url = r"https://en.wikipedia.org/wiki/List_of_stations_of_the_Paris_M%C3%A9tro"
 response = requests.get(url)
 soup_page = BeautifulSoup(response.content,"html.parser")
@@ -12,16 +13,17 @@ soup_table = soup_page.find('table')
 for soup_line in soup_table.findAll('tr'):
     indice = 0
     station= ""
-    linelist= []
+    lineslist= []
     for soup_case in soup_line.findAll('td'):
         for soup_station in soup_case.findAll('a'):
             if(indice == 0):
                 station = soup_station.string
             if(indice ==3):
-                linelist.append(soup_station.string)
+                lineslist.append(soup_station.string)
         indice += 1
     if not station == "":
-        tmp = [station, linelist]
-        data.append(tmp)
+        stationslist.append(station)
+        lineslistlist.append(lineslist)
+dic=dict(zip(stationslist, lineslistlist))
 
-print(data)
+print(dic)
