5028366132e51914d391e8f3dfe21b5a187e98e3
SY Samba <ssamba@localhost.localdomain>
2018-03-28 16:09:51 +0200

    Merge branches 'master' and 'sy' of https://github.com/rvm8h/python-exercice into sy
    # Conflicts:
    #	class_exemple.py

--- Metro_ss.py
+++ Metro_ss.py
@@ -1,25 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
+from bs4 import BeautifulSoup
+import requests
+
 url = r"https://en.wikipedia.org/wiki/List_of_stations_of_the_Paris_M%C3%A9tro"
 response = requests.get(url)
-soup_page = BeautifulSoup(response.content, "html.parser")
+soup = BeautifulSoup(response.content, "html.parser")
 
-the = soup_page.find_all('th')
-print(the)
+table = soup.find('table')
+#print(table)
 
-tre = soup_page.find_all('tr')
-print(tre)
-
-for line in the:
-       line = soup_page.find_all(the=['station', 'numero'])
-print(line)
-
-for line in tre:
-        line = soup_page.find_all(tre=['station', 'numero'])
-print(line)
-
-
+for tr in table.findAll('tr'):
+    for td in tr.findAll('td')[::3]:# en sautant le deux cases et prendre le 3
+        for a in td.findAll('a'):
+          print(a.text)
 # Trouver tous les tag th dans le site web
 # url = r"https://en.wikipedia.org/wiki/List_of_stations_of_the_Paris_M%C3%A9tro"
 # Dans ce site web travailler avec les tags th et tr pour retourner tous les noms des stations de metros et son numero de ligne associe.
