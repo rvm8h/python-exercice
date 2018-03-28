53e69b7f14d2455bcf3d423dd708ba6eacc967b6
Olivier <olivier.tran.al@gmail.com>
2018-03-28 14:20:43 +0200

    Search_metroline à peu près fonctionnel

--- Metro_OT.py
+++ Metro_OT.py
@@ -4,7 +4,7 @@
 
 from bs4 import BeautifulSoup
 import requests
-import re
+
 
 url = r"https://en.wikipedia.org/wiki/List_of_stations_of_the_Paris_M%C3%A9tro"
 
@@ -12,30 +12,16 @@ response = requests.get(url)
 print(response.encoding)
 soup_page = BeautifulSoup(response.content,"html.parser")
 #print(soup_page)
-regex = r"Paris Métro Line [0-9]*"
 
-metrolineregex = re.compile(regex)
-print(metrolineregex)
-
-metroline = soup_page.find_all(text=metrolineregex)
-
-print(metroline)
-
-#search_station = soup_page.find_all(["tr","div", "li","a"], class_ = "mw-redirect", recursive= True)
 search_lines = soup_page.find_all(["td","a"])
 
 
-#print("Search entries:\n",search_lines)
-
-#print(search_entries.th.string)
-
 for v in search_lines:
 
-    print("V = ",v)
-    if v.string != None:
+    #print("V = ",v)
+    if v.has_attr("title") and "(Paris Métro)" in v.get("title"):
+        print("Metro: ",v.string)
+    if v.has_attr('title') and "Paris Métro Line" in v.get("title") and v.string != None:
+        #print(v.get("title"))
         print("Line: ",v.string)
 
-#for v in search_station:
-
-#    if v.string != None:
-#        print("Station: ",v.string)
