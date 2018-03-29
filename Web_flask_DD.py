62909bc47a30a9d507959bfaea6a31ceb4353785
dimsdev <dimsdev@gmail.com>
2018-03-29 10:37:35 +0200

    modif de host

--- Web_flask_DD.py
+++ Web_flask_DD.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def index():
     return r'<h1>Hello World Python</h1>'
 
-## 
+##
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
