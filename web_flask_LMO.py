ab2f464a4a5cae04fd0cc0a354dc9d1babd05d6b
Laurent <laurent.monribot@gmail.com>
2018-03-30 11:23:09 +0200

    Décorations de fonctions pour récupérer les requètes du site web

--- web_flask_LMO.py
+++ web_flask_LMO.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello world !</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}<p>'.format(user_agent)
+    #return '<h1>Hello world !</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello {} !</h1>'.format(name)
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', debug=True)
