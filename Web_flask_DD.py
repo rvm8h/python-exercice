dc4767dfef9e0f0b1213e627bcb487a995e132b9
dimsdev <dimsdev@gmail.com>
2018-03-30 11:22:47 +0200

    modif de host

--- Web_flask_DD.py
+++ Web_flask_DD.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return r'<h1>Hello World Python</h1>'
 
-##
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello {} !</h1>'.format(name)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
