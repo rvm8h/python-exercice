30985f6b06289b8ed33ec3b43a116d5545ef53b7
Laurent <laurent.monribot@gmail.com>
2018-03-30 11:42:58 +0200

    Ouverture page web

--- web_flask_LMO.py
+++ web_flask_LMO.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import redirect, render_template
 
 app = Flask(__name__)
 
@@ -12,6 +13,14 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello {} !</h1>'.format(name)
+
+@app.route('/test/<name>')
+def test(name):
+    return render_template('index.html', name = name)
+
+@app.route('/site')
+def redir():
+    return redirect('https://www.python.org/')
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', debug=True)
