edcf84ca4b9c5ffa0b18af46db2faf1593017704
dimsdev <dimsdev@gmail.com>
2018-03-30 11:29:38 +0200

    modif de host

--- Web_flask_DD.py
+++ Web_flask_DD.py
@@ -1,15 +1,27 @@
 from flask import Flask
-
+from flask import request
+from flask import redirect
 
 app = Flask(__name__)
-
+'''
 @app.route('/')
 def index():
     return r'<h1>Hello World Python</h1>'
+'''
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return r'<p>Your browser is : {}</p>'.format(user_agent)
 
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello {} !</h1>'.format(name)
+
+
+@app.route('/site')
+def redir():
+    return redirect('https://www.python.org/')
 
 
 if __name__ == '__main__':
