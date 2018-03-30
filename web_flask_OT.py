6534bdd4eb8c758999f1473631124b99ed0f0701
Olivier <olivier.tran.al@gmail.com>
2018-03-30 11:27:44 +0200

    ajout de code web_flask 1.5

--- web_flask_OT.py
+++ web_flask_OT.py
@@ -1,18 +1,23 @@
 from flask import Flask
+from flask import request
+from flask import redirect
 
 app = Flask(__name__)
 
 #decorator
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<h1>Hello World!</h1>\n<p> Your Browser is {}</p>'.format(user_agent)
 
 
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello {} !</h1>'.format(name)
 
-
+@app.route('/site')
+def redir():
+    return redirect('https://www.python.org/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug = True)
